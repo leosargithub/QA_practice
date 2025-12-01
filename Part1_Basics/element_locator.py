035526ba425c7b0ff3197e6788222a0d934da908
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2025-12-01 15:15:53 +0545

    login system with by Name

--- Part1_Basics/element_locator.py
+++ Part1_Basics/element_locator.py
@@ -1,8 +1,34 @@
 from selenium import  webdriver
 import time
+from selenium.webdriver.common.by import By
 browser = webdriver.Chrome()
 browser.maximize_window()
 
 browser.get("https://www.saucedemo.com")
 
-time.sleep(5)
+time.sleep(2)
+
+#locate username web element
+username = browser.find_element(By.NAME, "user-name")
+
+
+#enter username
+username.send_keys("standard_user")
+
+time.sleep(2)
+
+password = browser.find_element(By.NAME, "password")
+
+
+#enter password
+password.send_keys("secret_sauce")
+
+
+#button click
+loginBtn = browser.find_element(By.NAME, "login-button")
+loginBtn.click()
+time.sleep(2)
+
+
+#close browser
+browser.quit()
