264e8c513b04a8717060f0f23517f6677522345f
Saroj Budhathoki <budhathokisaroj827@gmail.com>
2025-12-02 20:05:32 +0545

    add multiple checkbox using for loop

--- Part2_Intermediate/CheckBox.py
+++ Part2_Intermediate/CheckBox.py
@@ -17,18 +17,27 @@ print(f"window {title}")
 #fetch url and print
 print("url:", browser.current_url)
 
+#
+# #find check box and select checkbox
+# checkbox_ford = browser.find_element(By.ID, "option1")
+#
+# if not checkbox_ford.is_selected():
+#     checkbox_ford.click()
+#
+# checkbox_bmw = browser.find_element(By.ID, "option2")
+# checkbox_bmw.click()
+#
+# checkbox_mercedes= browser.find_element(By.ID, "option3")
+# checkbox_mercedes.click()
+#
+# #veryfy selection
+# print("is Ford selected ", checkbox_bmw.is_selected())
+# print("is Mercedes selected", checkbox_mercedes.is_selected())
 
-#find check box and select checkbox
-
-checkbox_bmw = browser.find_element(By.ID, "option2")
-checkbox_bmw.click()
-
-checkbox_mercedes= browser.find_element(By.ID, "option3")
-checkbox_mercedes.click()
-
-#veryfy selection
-print("is Ford selected ", checkbox_bmw.is_selected())
-print("is Mercedes selected", checkbox_mercedes.is_selected())
+# find elements and check multiple checkboxes
+checkboxes = browser.find_elements(By.XPATH, "//input[@type='checkbox']")
+for checkbox in checkboxes:
+    checkbox.click()
 
 time.sleep(5)
 browser.quit()
